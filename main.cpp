23f6808ad814d35b5d1a5a93387dfe151a428f55
kirill <kirill@kirill.kirill>
2026-03-17 11:10:35 +0300

    another commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "vec.hpp"
+
+bool testEmptVec(){
+  return 0;
+}
 
 int main() {
-  std::cout << 1;
+  using test_t = std::pair<const char *, bool(*)() >; 
+  test_t tests[] = {
+    {"Empty vector", testEmptVec}
+  };
+  const size_t count = sizeof(tests)/sizeof(test_t);
+  std::cout << std::boolalpha;
+  for (size_t i = 0; i < count; ++i) {
+    std::cout << tests[i].first << ": " << tests[i].second() << '\n';
+  }
 }
--- vec.hpp
+++ vec.hpp
@@ -1,10 +1,24 @@
-#ifdef VEC_HPP
 #define VEC_HPP
+#ifdef VEC_HPP
 
+namespace topit {
+  template<class T>
+  struct vec {
+    vec();
+    ~vec();
+  private:
+    T* data_;
+    size_t size_, cap_;
+  };
+}
 template<class T>
-struct vec {
-  T* data;
-  size_t size, cap;
-};
+topit::vec<T>::~vec()
+{}
+template<class T>
+topit::vec<T>::vec():
+  data_(),
+  size_(),
+  cap_()
+{}
 
 #endif
