4caea529a6e5c982718ac18c3c77980bfd7156d7
kirill <kirill@kirill.kirill>
2026-03-27 23:31:31 +0300

    sync code which i not add in practics

--- main.cpp
+++ main.cpp
@@ -2,23 +2,120 @@
 #include <iomanip>
 #include "vec.hpp"
 
-bool testEmptVec(){
+bool testEmptVector(){
   topit::vec<int> v;
-  return v.is_empty();
+  return v.isEmpty();
+}
+
+bool testElementAccess() {
+  topit::vec<int> v;
+  v.pushBack(1);
+  try {
+    int& val = v.at(0);
+    return val == 1;
+  } catch (...) {
+    return 0;
+  }
+}
+
+bool testElementOutOfBoundAccess() {
+  topit::vec<int> v;
+  try {
+    int & val = v.at(0);
+    return 0;
+  } catch (const std::out_of_range &) {
+    return 1;
+  } catch (...) {
+    return 0;
+  }
+}
+
+bool testElementInOfBoundAccess() {
+  topit::vec<int> v;
+  v.pushBack(1);
+  topit::vec<int> rv = v;
+  try {
+    const int & val = v.at(0);
+    return val == 1;
+  } catch (...) {
+    return 0;
+  }
+}
+
+
+bool testElementInboundConstAccess() {
+  topit::vec<int> v;
+  v.pushBack(1);
+  const topit::vec<int>& rv = v;
+  try {
+    const int& val = rv.at(0);
+    return val == 1;
+  } catch (...) {
+    return false;
+  }
+}
+
+
+bool testElementOutOfBoundConstAccess() {
+  topit::vec<int> v;
+  const topit::vec<int>& rv = v;
+  try {
+    rv.at(0);
+    return false;
+  } catch (const std::out_of_range &) {
+    return true;
+  } catch (...) {
+    return false;
+  }
+}
+
+bool testCopyConstructor() {
+  topit::vec<int> v;
+  topit::vec<int> yav{v};
+  return v == yav;
+}
+
+bool testCopyConstructorForEmpty() {
+  topit::vec<int> v;
+  topit::vec<int> yav = v;
+  return v == yav;
+}
+
+bool testCopyConstructorForNonEmpty() {
+  topit::vec<int> v;
+  v.pushBack(1);
+  topit::vec<int> yav = v;
+  try {
+    return yav.getSize() == v.getSize() && yav.at(0) == v.at(0);
+  } catch (...) {
+    return 0;
+  }
 }
 
 int main() {
-  using test_t = std::pair<const char *, bool(*)() >; 
+  using test_t = std::pair<const char *, bool(*)() >;
+
   test_t tests[] = {
-    {"Empty vector", testEmptVec}
+    {"Empty vector", testEmptVector},
+    {"Element access", testElementAccess},
+    {"Inbound access", testElementInOfBoundAccess},
+    {"Out Of bound access", testElementOutOfBoundAccess},
+    {"Inbound const access", testElementInboundConstAccess},
+    {"Out of bound const access", testElementOutOfBoundConstAccess},
+    {"Copy empty vector", testCopyConstructorForEmpty},
+    {"Copy non-empty vector", testCopyConstructorForNonEmpty},
   };
+
   const size_t count = sizeof(tests)/sizeof(test_t);
   std::cout << std::boolalpha;
   bool pass = 1;
+
   for (size_t i = 0; i < count; ++i) {
     bool res = tests[i].second();
-    std::cout << tests[i].first << ": " << tests[i].second() << '\n';
+    std::cout << tests[i].first << ": " << res << '\n';
     pass = pass && res;
   }
   std::cout << "Result: " << pass << "\n";
+
+  return 0;
 }
